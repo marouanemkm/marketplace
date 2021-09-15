31d8835be837e37e465dfe6cc01d81311a28eec5
Marouane <maroumkm@gmail.com>
2021-09-15 21:50:36 +0200

    Mise en place du projet

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -67,8 +67,8 @@ class RegisterController extends Controller
         return User::create([
             'name' => $data['name'],
             'email' => $data['email'],
-            'password' => Hash::make($data['password']),
             'role' => User::USER_ROLE,
+            'password' => Hash::make($data['password']),
         ]);
     }
 }
